cca5d550a0a632009bf19eec11441de9ba441bdb
mateogolf <rodriguezml11@mail.wlu.edu>
2017-10-11 16:29:03 -0400

    Switchboard assignment completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  switches = [];
+  constructor(){
+    for(let i=0;i<10;i++){
+      this.switches.push(false);
+    }
+  }
+  flipSwitch(idx):void{
+    if(this.switches[idx]){
+      this.switches[idx] = false;
+      return;
+    }
+    this.switches[idx] = true;
+  }
 }
